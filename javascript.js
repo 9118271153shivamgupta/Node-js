f7d679d10b8587a320b56e7ceee4eac6a5fcb9f3
Shivam Gupta <shivamkumargupta5592@gmail.com>
2024-10-08 11:02:37 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+console.log("start");
+console.log("middle");
+console.log("end");
+
+
+// output   =>
+    // start 
+    // middle
+    // end
+
+console.log("start");
+console.log("middle");
+
+console.log("end");
