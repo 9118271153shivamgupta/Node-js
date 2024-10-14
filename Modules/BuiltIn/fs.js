1918fd2db474a52a81acc572ba1f4a1d65e7e5d9
Shivam Gupta <shivamkumargupta5592@gmail.com>
2024-10-14 10:16:44 +0530

    all setup

--- Modules/BuiltIn/fs.js
+++ Modules/BuiltIn/fs.js
@@ -1,43 +1,108 @@
-// syntex to import built-in modules
-// let variale-name = require("node:module-name")
-//? "node:" ==> itis not mondatory, we can remove this if we want
-// module-name ==> fs, https,crypto, os
+// syntax to import built-in modules
+// let variable-name = require(" node:module-name ")
+//? "node:" ==> it is not mandatory, we can remove this if we want.
+// module-name ==> fs, http, crypto, os.
 
 let fs = require("node:fs");
 // console.log(fs);
 
-// ! fs===> it stand for file system. This modules provide us some functionality to perform CRUD of files present on our os.
+//! fs ==> it stands for file system. this module provides us some functionality to perform CRUD on files present on our OS. and also we can interact with directories/folder.
 
-// CRUD  on files using Synchronous functions.
+//? CRUD ==> create, read, update, delete
 
-// creating a file synchronously======
+//! ===================== CRUD on files using Synchronous functions
 
-// method name===> readFileSync()
-// syntex=====> fs.readFileSync("path", "./encoding")
+//! 1) ==== reading a file synchronously
+// method name ==> readFileSync()
+// syntax ==> fs.readFileSync("path", "encoding")
 
-// ex:
+/* console.log("Start");
+console.log("middle");
 
+// let data = fs.readFileSync("./index.html");
+// console.log(data.toString()); // this will return a buffer value (array of binary numbers). to convert buffer to string format use toString().
 
+let data = fs.readFileSync("./index.html", "utf-8"); // "utf-8" ==> represents string format
+console.log(data);
+
+console.log("end"); */
+
+// let data = fs.readFileSync("../UserDefined/index.js", "utf-8"); // "utf-8" ==> represents string format
+// console.log(data);
+
+//! 2)============== creating a file synchronously
+// method name ==> writeFileSync()
+// syntax ==> fs.writeFileSync("path/file-name", "data to be written")
 
 // console.log("start");
-// console.log("Middle");
+// console.log("middle");
 
-// // let data = fs.readFileSync("./index.html")
-// // console.log(data);// this will return a buffer value (Array if binary numbrs). to convert buffer to string formate ue=se tostring.
-// let data1 = fs.readFileSync("./index.html", "utf-8") // utf-8 it represent to string formate
-// console.log(data1);
+// fs.writeFileSync("../../data.txt", "console.log(`hi`)");
+// console.log("file created");
 
 // console.log("end");
 
+// ? if file-name does not exists it will create a new file with provided data, otherwise it will simply over-write the existing data.
 
-// =================================================================
-// 2!===================== 2 way  to creating file synchronously
-// method name ===writefileSync()
-// syntex====> fs.writeFileSync("path/file-name", "data to written")
-console.log("St");
+//! 3) ============== updating a file synchronously
+// method name ==> appendFileSync()
+// syntax ==> fs.appendFileSync("path/file-name", "new data")
+
+// fs.appendFileSync("./data.txt", "new data");
+// console.log("file updated");
+
+//? if the file-name does not exists it will create a new file with provided data, if the file is present it will simply append the data.
+
+//! 4)================= deleting a file synchronously
+// method name ==> unlinkSync()
+// syntax ==> fs.unlinkSync("path/file-name")
+
+/* console.log("start");
 console.log("middle");
-fs.writeFileSync("./data.js", "console.log(`hi`)");
-console.log("file created");
-console.log("ed");
 
-// if file name does not exists it will create a new file with provide data , otherwise it will simply over-write the wexisting file ..
+fs.unlinkSync("./server.js");
+
+console.log("end");
+ */
+
+//! 5) =================== renaming a file/folder sync
+// method name ==> renameSync()
+// syntax ==> fs.renameSync("old-file-name", "new-file-name")
+
+// fs.renameSync("../../index.html", "index.csv");
+// console.log("file renamed");
+
+//? copy the contents of "fs.js" and paste it in new file "fs2.js"
+// let readFile = fs.readFileSync("./fs.js", "utf-8");
+// fs.writeFileSync("./fs2.js", readFile);
+// console.log("data copied");
+
+//! 6) ============= creating a directory/folder synchronously
+// method-name ==> mkdirSync()
+// syntax ==> fs.mkdirSync("path/folder-name")
+
+// fs.mkdirSync("./newFolder");
+// console.log("folder created");
+
+//! 7) ============= removing a directory/folder synchronously
+// method-name ==> rmdirSync()
+// syntax ==> fs.rmdirSync("path/folder-name")
+
+// fs.rmdirSync("./newFolder");
+// console.log("folder removed");
+
+//! src >> controllers >> userController >> app.js
+//! creating
+// fs.mkdirSync("./src");
+// console.log("src created");
+// fs.mkdirSync("./src/controllers");
+// console.log("controllers created");
+// fs.mkdirSync("./src/controllers/userController");
+// console.log("userController created");
+// fs.writeFileSync("./src/controllers/userController/app.js", "console.log(`hello`)");
+
+//! deleting
+// fs.unlinkSync("./src/controllers/userController/app.js");
+// fs.rmdirSync("./src/controllers/userController");
+// fs.rmdirSync("./src/controllers");
+// fs.rmdirSync("./src");
--- basic.js
+++ basic.js
@@ -1,10 +1,10 @@
-// modules in js====>it is logical piece of code which can be reuse multiple time.
-// using modules we can esily break our code into smaller and resuable parts
-//  it incrrease code visibility and maintainnablity..
-//  it helps us to avoide code dublication .(DRY principle==> Don't repeate yourself)
+/* modules in js====>it is logical piece of code which can be reuse multiple time.
+    2==> using modules we can esily break our code into smaller and resuable parts
+    3==> it incrrease code visibility and maintainnablity..
+    4==> it helps us to avoide code dublication .(DRY principle==> Don't repeate yourself)
+ */
 
-
-// ! in node JS we have three type of Modules == Bulitin , User DEfine and third Party Modules.
+// ! in node JS we have three type of Modules == Bulitin , User Define and third Party Modules.
 
 // ? User-Define:==>These modules are created by user. also known as local modules.
 
